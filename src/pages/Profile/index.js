fae456be860b7918149a91c85536740fcb51aa7c
Darshan <93696894+darshan253@users.noreply.github.com>
2024-05-06 12:17:13 +0530

    Add files via upload

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,13 @@
+import React from'react'
+import PageTitle from '../../components/PageTitle'
+
+function Profile() {
+    return (
+      <div>
+        <PageTitle title="Profile" />
+        <div>Bookings Information</div> 
+    </div>
+    )
+  }
+
+  export default Profile;  
